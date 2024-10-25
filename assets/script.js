fce7ecd428f4d3b5c5b3b1795416eaf53a91b5e8
relativityparadox <malcolmshaywood@gmail.com>
2024-10-25 14:56:52 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,1 @@
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('header');
-    if (window.scrollY > 50) {
-        header.classList.remove('transparent');
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-        header.classList.add('transparent');
-    }
-});
+
