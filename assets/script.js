2427d6c6229d5a68e8dc8725c6c5885d646543c2
relativityparadox <malcolmshaywood@gmail.com>
2024-10-25 15:02:25 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
+function loadLinks() {
+    fetch('links.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('links-placeholder').innerHTML = data;
+        });
+}
 
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+// Load the links and other dynamic content
+loadLinks();
+loadHTML('header-placeholder', 'header.html');
+loadHTML('footer-placeholder', 'footer.html');
